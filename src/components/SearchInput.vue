02e42c80bc567be6568378545e1590ee7897f4d1
Kostas V <konsvass@outlook.com>
2019-10-16 13:51:12 +0200

    Added icons for searchbar, styled searchbar and added api call onClick

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,12 +1,30 @@
 <template>
-  <div class="search-input-container">
-    <input placeholder="How can we make you laugh today?"/>
+  <div class="search-input">
+    <div class="search-input__container">
+      <input type="text" v-model="query" placeholder="How can we make you laugh today?"/>
+      <button class="search-input__button" @click="searchForJoke"></button>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'SearchInput',
+  
+  data: function() {
+    return {
+      query: '',
+    }
+  },
+
+  methods: {
+    async searchForJoke() {
+      const response = await fetch(`https://api.chucknorris.io/jokes/search?query=${this.query}`);
+      const data = await response.json();
+      console.log('Click', data);
+    }
+  }
 }
 </script>
 
@@ -14,7 +32,7 @@ export default {
   $input-color: #b2b2b2;
   $input-color__focus: #000000;
 
-  .search-input-container {
+  .search-input {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -28,7 +46,12 @@ export default {
     background-size: cover;
     min-height: 140px;
 
-    input {
+    &__container {
+      position: relative;
+      width: 100%;
+    }
+
+    input[type="text"] {
     border-radius: 4px;
     border: solid 1px #fff;
     background: transparent;
@@ -41,20 +64,50 @@ export default {
         background: white;
         color: $input-color__focus;
       }
+
+      &:focus + button {
+        background-image: url('../assets/search-icon__black.svg');
+        background-repeat: no-repeat;
+        background-color: transparent;
+        background-size: cover;
+        border: none;
+      }
     }
 
     ::placeholder {
       color: $input-color;
     }
 
+    &__button {
+      position: absolute;
+      top: 5px;
+      right: 5px;
+      background-image: url('../assets/search-icon.svg');
+      background-repeat: no-repeat;
+      background-color: transparent;
+      background-size: cover;
+      border: none;
+      width: 16px;
+      height: 16px;
+    }
+
     @media (min-width: 600px) {
       min-height: 240px;
-
-      input {
+      &__container {
+        width: auto;
+      }
+      input[type="text"] {
         width: 450px;
         height: 48px;
         font-size: 15px;
         padding: 14px 16px;
+      }
+
+      &__button {
+        width: 24px;
+        height: 24px;
+        top: 14px;
+        right: 16px;
       }
     }
   }
